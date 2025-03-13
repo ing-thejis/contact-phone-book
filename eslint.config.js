34e757acc837ef410784b4e967d85461eae32c07
Jesith Garcia <ing.jesithgarcia@gmail.com>
2025-03-13 09:38:14 -0500

    feat: adjust eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,49 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import tsParser from "@typescript-eslint/parser"
+import unusedImports from "eslint-plugin-unused-imports"
+import airbnbBase from "eslint-config-airbnb-base"
+import airbnbBaseTypescript from "eslint-config-airbnb-typescript"
+import js from "@eslint/js"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {rules: {
-    "react/react-in-jsx-scope": "off",
-		"react/jsx-uses-react": "off",
-  }}
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      }
+    },
+    plugina: {
+      "@typescript-eslint": tseslint,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      ...airbnbBase,
+      ...airbnbBaseTypescript,
+      "no-unused-vars": "off",
+       "react/react-in-jsx-scope": "off",
+      "unused-imports/no-unused-imports": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          caghtErrorsIgnorePattern: "^_",
+        }
+      ],
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+        }
+      }
+    }
+  },
 ];
